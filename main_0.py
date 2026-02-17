116d55ee617ab14def505ad13e18bd0962e634e9
Derek <ushakov_gd@mail.ru>
2026-02-18 02:33:57 +0300

    Добавил класс хозяин

--- main_0.py
+++ main_0.py
@@ -9,6 +9,17 @@
 # - Вес: вещественное число (в килограммах)
 # - Вакцинирован: булево значение (да/нет)
 
+
+# Класс хозяин
+class Owner:
+
+    name: str
+
+    def __init__(self, name: str):
+
+        self.name = name
+
+# Класс животное
 class Pet:
 
     name: str
@@ -16,14 +27,16 @@ class Pet:
     age: int
     weight: float
     is_vaccine: bool
+    owner: str
 
-    def __init__(self, name: str, view: str, age: int, weight: float, is_vaccine: bool):
+    def __init__(self, name: str, view: str, age: int, weight: float, is_vaccine: bool, owner=Owner):
 
         self.name = name
         self.view = view
         self.age = age
         self.weight = weight
         self.is_vaccine = is_vaccine
+        self.owner = owner
 
         if age <= 0:
             raise ValueError("Возраст питомца, не может быть меньше или равным нулю")
