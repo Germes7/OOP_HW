bf1f45ae668a0c19b20de1fa13cb10f9fbe7208f
Derek <ushakov_gd@mail.ru>
2026-02-18 01:26:07 +0300

    Добавил класс Pet

---
+++ main_0.py
@@ -0,0 +1,28 @@
+# Задание №3
+
+# Задача №1
+# **Характеристики животных**
+#
+# - Имя: строка
+# - Вид: строка (например, собака, кошка, попугай)
+# - Возраст: целое число (в годах)
+# - Вес: вещественное число (в килограммах)
+# - Вакцинирован: булево значение (да/нет)
+
+class Pet:
+
+    name: str
+    view: str
+    age: int
+    weight: float
+    is_vaccine: bool
+
+    def __init__(self, name: str, view: str, age: int, weight: float, is_vaccine: bool):
+
+        self.name = name
+        self.view = view
+        self.age = age
+        self.weight = weight
+        self.is_vaccine = is_vaccine
+
+    
