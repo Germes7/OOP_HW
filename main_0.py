890c3ac17a3d1d8919056d27f29e253368a471fd
Derek <ushakov_gd@mail.ru>
2026-02-18 03:18:53 +0300

    Добавил метод звуков издаваемые животным

--- main_0.py
+++ main_0.py
@@ -4,7 +4,7 @@
 # **Характеристики животных**
 #
 # - Имя: строка
-# - Вид: строка (например, собака, кошка, попугай)
+# - Вид: строка (например, собака, кошка, попугай, удав)
 # - Возраст: целое число (в годах)
 # - Вес: вещественное число (в килограммах)
 # - Вакцинирован: булево значение (да/нет)
@@ -27,19 +27,37 @@ class Pet:
     age: int
     weight: float
     is_vaccine: bool
-    owner: str
+    lord: Owner
 
-    def __init__(self, name: str, view: str, age: int, weight: float, is_vaccine: bool, owner=Owner):
+    def __init__(self, name: str, view: str, age: int, weight: float, is_vaccine: bool, lord: Owner):
 
         self.name = name
         self.view = view
         self.age = age
         self.weight = weight
         self.is_vaccine = is_vaccine
-        self.owner = owner
+        self.lord = lord
 
         if age <= 0:
             raise ValueError("Возраст питомца, не может быть меньше или равным нулю")
 
         if weight <= 0:
             raise ValueError("Вес не может быть меньшим либо равным нулю")
+
+    def action(self):
+
+        if self.view.lower() == "удав":
+            return f"{self.view} шипит"
+
+        elif self.view.lower() == "пес":
+            return f"{self.view} лает"
+
+        elif self.view.lower() == "кот":
+            return f"{self.view} мяукает"
+
+        elif self.view.lower() == "попугай":
+            return f"{self.view} кричит Полундра!"
+
+        else:
+            return f"{self.view} что-то орет"
+
